6a05dbea3431fca40ec60b254a3723fedefe911d
Juraj Štefanić <jurajstefanic@outlook.com>
2026-05-14 01:00:29 +0200

    feat: add command detail prose

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,14 @@
 import { Detail } from "@raycast/api";
 
 export default function Command() {
-  return <Detail markdown="# Run AI Code" />;
+  return (
+    <Detail
+      markdown={[
+        "# Daytona",
+        "",
+        "Daytona Extension for Raycast.",
+        "",
+      ].join("\n")}
+    />
+  );
 }
